4cd9279a6a0e0550133eef249027b6d1bd74f7b1
elhispanopolako <tadeo98hiszpan@gmail.com>
2022-01-05 14:29:31 +0100

    Add ResultsPage

---
+++ src/test/java/pl/justjoindemo/pages/ResultsPage.java
@@ -0,0 +1,52 @@
+package pl.justjoindemo.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.io.IOException;
+import java.util.List;
+
+
+public class ResultsPage {
+    @FindBy(xpath="//div[contains(text(),'Tester')]")
+    private List<WebElement> jobOffers;
+    @FindBy(xpath ="//*[@id='offer-apply-container']/form/div[1]/div[1]/div/div[2]/div/input" )
+    private WebElement nameInput;
+    @FindBy(xpath ="//*[@id='offer-apply-container']/form/div[1]/div[2]/div/div[2]/div/input" )
+    private WebElement emailInput;
+    @FindBy(xpath ="//*[@id='offer-apply-container']/form/div[1]/div[3]/div[2]/div/textarea[1]" )
+    private WebElement textInput;
+    @FindBy(xpath = "//*[@id='offer-apply-container']/form/div[1]/div[4]/div/div[2]//input")
+    private WebElement cvUpload;
+    @FindBy(xpath ="//*[@id='offer-apply-container']/form/div[1]/div[4]/div/div[2]//input")
+    private WebElement sendApply;
+
+
+
+    public ResultsPage(WebDriver driver) {
+        PageFactory.initElements(driver,this);
+    }
+
+    public void chooseJobOffers(int number){
+        jobOffers.get(number).click();
+    }
+    public void setForm(String name,String email){
+        nameInput.sendKeys(name);
+        emailInput.sendKeys(email);
+    }
+    public void setCvUpload(){
+
+        cvUpload.sendKeys("C:\\Users\\Toshiba\\Downloads\\CVUA1.pdf");
+    }
+
+    public boolean setTextInput() {
+        textInput.isEnabled();
+        return false;
+    }
+
+    public void sendApply(){
+        sendApply.click();
+    }
+}
--- src/test/java/pl/justjoindemo/tests/JuniorTesterTest.java
+++ src/test/java/pl/justjoindemo/tests/JuniorTesterTest.java
@@ -1,7 +1,9 @@
 package pl.justjoindemo.tests;
 
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.Test;
 import pl.justjoindemo.pages.HeaderPage;
+import pl.justjoindemo.pages.ResultsPage;
 
 public class JuniorTesterTest extends BaseTest {
     @Test
@@ -10,19 +12,12 @@ public class JuniorTesterTest extends BaseTest {
         headerPage.setTestTech();
         headerPage.setJuniorLevel();
         headerPage.setShowOffer();
+        ResultsPage resultsPage=new ResultsPage(driver);
+        resultsPage.chooseJobOffers(1);
 
-      /*
-       WebElement oferChoose=driver.findElement(By.xpath("//*[@id='root']/div[2]/div[1]/div/div[2]/div[1]/div/div/div[4]/a/div/div[2]/div[1]/div[1]/div"));
-       oferChoose.click();
-       Thread.sleep(1000);
-       WebElement nameInput=driver.findElement(By.xpath("//*[@id='offer-apply-container']/form/div[1]/div[1]/div/div[2]/div/input"));
-       nameInput.sendKeys("Tadeusz Woźniak");
-       WebElement emailInput= driver.findElement(By.xpath("//*[@id='offer-apply-container']/form/div[1]/div[2]/div/div[2]/div/input"));
-       emailInput.sendKeys("tadeo98hiszpan@gmail.com");
-       WebElement textInput= driver.findElement(By.xpath("//*[@id='offer-apply-container']/form/div[1]/div[3]/div[2]/div/textarea[1]"));
-       textInput.sendKeys("Dzień dobry, z góry prepraszam za tą aplikację jestem w trakcie nauki selenium i tą aplikację wysyłam automatycznie. Byłbym bardzo zadowolony i wdzięczny gdybym dostał od państwa odpowiedź z informacją czy doszło poprawnie oraz co poprawić w kodzie link do aktualnego testu w github-->https://github.com/elhispanopolako/Selenium-tutorial/blob/5eaa123be74e11562812b644e1084a0f91246526/src/test/java/JustJoinTest.java oraz mój linkedin-->https://www.linkedin.com/in/tadeusz-wo%C5%BAniak-3162a217a/  pozdrawiam Miłego dnia Tadeusz Woźniak");
-       WebElement sendApply= driver.findElement(By.xpath("//*[@id=\"offer-apply-container\"]/form/div[3]/div[1]/button/span[1]"));
-       sendApply.click();*/
+        resultsPage.setForm("Tadeusz Woźniak", "tadeo98hiszpan@gmail.com");
+        resultsPage.setCvUpload();
+
 
     }
 }
